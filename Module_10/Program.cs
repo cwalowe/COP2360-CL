aa84839480990d5ab1710b6879966a8ecaca135c
cam <73373703+cwalowe@users.noreply.github.com>
2024-11-29 23:54:40 -0500

    Update Program.cs
    added all the main functionality to get inputs for the class constructors

--- Module_10/Program.cs
+++ Module_10/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Project10
+using System.Numerics;
+
+namespace Project10
 {
     class Program
     {
@@ -6,7 +8,32 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter contractor name.");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter contractor number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the start date.");
+            DateTime start = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 1 for day shift. Enter 2 for night shift.");
+            int shift = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the hourly wage.");
+            double payrate = Convert.ToDouble(Console.ReadLine());
+            Subcontractor sub1 = new Subcontractor(userName, number, start, shift, payrate);
+
+            double PayrollCalc(double payrate, int shift, DateTime start)
+            {
+                double pay = payrate * (DateTime.Now - start).TotalHours;
+                if (shift == 2)
+                {
+                    pay *= 1.03;
+                }
+                    return pay;
+            }
+            double payroll = PayrollCalc(payrate, shift, start);
+            Console.WriteLine($"{userName} earned ${payroll:F2} in this pay period.");
+            
         }
+
+
     }
 }
