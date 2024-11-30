5baf4e7c3faa1daec9609c8e2f3f4a1c36af2931
cam <73373703+cwalowe@users.noreply.github.com>
2024-11-29 23:57:01 -0500

    Update Subcontractor.cs
    added inherited constructor

--- Module_10/Subcontractor.cs
+++ Module_10/Subcontractor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Project10
 {
@@ -6,5 +6,11 @@ namespace Project10
     {
         public int shift;
         public double payrate;
+        public Subcontractor(string name, int number, DateTime startdate, int shift, double payrate): base(name, number, startdate)
+        {
+            this.shift = shift;
+            this.payrate = payrate;
+        }
+
     }
 }
